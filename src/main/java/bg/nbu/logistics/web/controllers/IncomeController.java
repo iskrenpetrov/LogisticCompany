53e57fcb032317782b968b6861b22a0397371de6
Kristina Krastanova <kristina.krastanova28@gmail.com>
2022-01-29 00:30:14 +0200

    Income html created

--- src/main/java/bg/nbu/logistics/web/controllers/IncomeController.java
+++ src/main/java/bg/nbu/logistics/web/controllers/IncomeController.java
@@ -4,11 +4,12 @@ import static bg.nbu.logistics.commons.constants.AuthorizationConstants.IS_AUTHE
 
 import java.time.LocalDate;
 
+import org.apache.tomcat.jni.Local;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
 import bg.nbu.logistics.services.income.IncomeService;
@@ -23,15 +24,20 @@ public class IncomeController extends BaseController {
         this.incomeService = incomeService;
     }
 
+    @PostMapping
+    @PreAuthorize(IS_AUTHENTICATED)
+    public ModelAndView fetchIncome(@RequestParam(name = "fromDate") String fromStr,
+                                    @RequestParam (name = "toDate") String toStr, ModelAndView modelAndView) {
+        LocalDate fromDate = LocalDate.parse(fromStr);
+        LocalDate toDate = LocalDate.parse(toStr);
+        final double income = incomeService.getIncomeByTimePeriod(fromDate, toDate);
+        modelAndView.addObject("income", income);
+        return view("income", modelAndView);
+    }
+
     @GetMapping
     @PreAuthorize(IS_AUTHENTICATED)
     public ModelAndView fetchIncome(ModelAndView modelAndView) {
-        final double income = incomeService.getIncomeByTimePeriod(LocalDate.now()
-                .minusMonths(2),
-                LocalDate.now()
-                        .plusMonths(3));
-
-        modelAndView.addObject("income", income);
-        return view("income", modelAndView);
+        return view("income");
     }
 }
